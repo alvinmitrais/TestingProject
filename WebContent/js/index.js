024dd620ad21d9861deccb75319252e099573c57
Alvin_S3106 <Alvin_S3106@MTPC539.mitrais.com>
2017-12-07 10:00:42 +0700

    commit lagi

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -5,7 +5,21 @@ function onClickButton(){
 	document.getElementById("desc").innerHTML = "Non-Boring Text here";
 }
 
-$('#form1').on('submit', function () {
-    alert('Form submitted!');
-    return false;
-});
+//$('#form1').on('submit', function () {
+//	
+//	
+//    alert('Form submitted!');
+//    return false;
+//});
+
+function validateInputs(){
+	var reg = new RegExp("([A-Z]|[a-z])");
+	var firstName = document.getElementById("txt1");
+	var lastName = document.getElementById("txt2");
+	
+}
+
+function test(){
+	validateInputs();
+	return false;
+}
